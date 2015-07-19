d963ad003da2f225bf46637dcae7c1d6a6b70c2f
Victoria Betts <victoriabetts@Perseus.local>
2015-07-19 16:25:52 -0400

    Adding staticfiles directory

--- Books/settings.py
+++ Books/settings.py
@@ -116,12 +116,11 @@ ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join('', 'BooksApp/static'),
+    os.path.join(BASE_DIR, 'static'),
 )
 
 
