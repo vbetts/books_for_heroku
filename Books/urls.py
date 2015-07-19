3124e99edb7f6f2b60735861015a883576bfe0fa
Victoria Betts <victoriabetts@Perseus.local>
2015-07-19 16:52:07 -0400

    static files directories

--- Books/urls.py
+++ Books/urls.py
@@ -15,7 +15,13 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.conf import settings
 
 urlpatterns = [
     url(r'^', include('BooksApp.urls'))
 ]
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
